38d71d2e6b9062bfa2d24c0ded8c88c4e6b83657
Nadav Tasher <tashernadav@gmail.com>
2019-05-06 15:40:40 +0300

    Initial commit

---
+++ scripts/backend/accounts/api.php
@@ -0,0 +1,224 @@
+<?php
+const DATABASE = __DIR__ . "/../../../files/accounts/database.json";
+const LOCKOUT_ATTEMPTS = 5;
+const LOCKOUT_TIME = 5 * 60;
+const MINIMUM_PASSWORD_LENGTH = 8;
+const REGISTER_ENABLED = true;
+
+$database = json_decode(file_get_contents(DATABASE));
+$result = new stdClass();
+
+function accounts()
+{
+    if (isset($_POST["action"])) {
+        $action = $_POST["action"];
+        if (isset($_POST[$action])) {
+            $parameters = json_decode($_POST[$action]);
+            switch ($action) {
+                case "login":
+                    if (isset($parameters->name) && isset($parameters->password))
+                        login($parameters->name, $parameters->password);
+                    else
+                        error("login", "Missing information");
+                    break;
+                case "register":
+                    if (isset($parameters->name) && isset($parameters->password))
+                        register($parameters->name, $parameters->password);
+                    else
+                        error("register", "Missing information");
+                    break;
+                case "verify":
+                    if (isset($parameters->certificate))
+                        return verify($parameters->certificate);
+                    else
+                        error("verify", "Missing information");
+                    break;
+            }
+            save();
+        }
+    }
+    return null;
+}
+
+function certificate()
+{
+    global $database;
+    $random = random(64);
+    foreach ($database as $id => $account) {
+        foreach ($account->certificates as $certificate) {
+            if ($certificate === $random) return certificate();
+        }
+    }
+    return $random;
+}
+
+function error($type, $message)
+{
+    result("errors", $type, $message);
+}
+
+function filter($source)
+{
+    // Filter inputs from XSS and other attacks
+    $source = str_replace("<", "", $source);
+    $source = str_replace(">", "", $source);
+    return $source;
+}
+
+function hashed($password, $saltA, $saltB)
+{
+    return hash("sha256", $saltA . $password . $saltB);
+}
+
+function id()
+{
+    global $database;
+    $random = random(10);
+    foreach ($database as $id => $account) {
+        if ($id === $random) return id();
+    }
+    return $random;
+}
+
+function lock($id)
+{
+    global $database;
+    $database->$id->lockout->attempts++;
+    if ($database->$id->lockout->attempts >= LOCKOUT_ATTEMPTS) {
+        $database->$id->lockout->attempts = 0;
+        $database->$id->lockout->time = time() + LOCKOUT_TIME;
+    }
+}
+
+function lockout($id)
+{
+    global $database;
+    return isset($database->$id->lockout->time) && $database->$id->lockout->time > time();
+}
+
+function login($name, $password)
+{
+    global $database;
+    $found = false;
+    result("login", "success", false);
+    foreach ($database as $id => $account) {
+        if ($account->name === $name) {
+            $found = true;
+            if (!lockout($id)) {
+                if (password($id, $password)) {
+                    $certificate = certificate();
+                    array_push($account->certificates, $certificate);
+                    result("login", "certificate", $certificate);
+                    result("login", "success", true);
+                } else {
+                    lock($id);
+                    error("login", "Incorrect password");
+                }
+            } else {
+                error("login", "Account locked");
+            }
+        }
+    }
+    if (!$found)
+        error("login", "Account not found");
+}
+
+function name($name)
+{
+    global $database;
+    foreach ($database as $id => $account) {
+        if ($account->name === $name) return true;
+    }
+    return false;
+}
+
+function password($id, $password)
+{
+    global $database;
+    return hashed($password, $database->$id->saltA, $database->$id->saltB) === $database->$id->hashed;
+}
+
+function random($length)
+{
+    $current = str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")[0];
+    if ($length > 0) {
+        return $current . random($length - 1);
+    }
+    return "";
+}
+
+function register($name, $password)
+{
+    global $database;
+    result("register", "success", false);
+    if (REGISTER_ENABLED) {
+        if (!name($name)) {
+            if (strlen($password) >= MINIMUM_PASSWORD_LENGTH) {
+                $account = new stdClass();
+                $account->certificates = array();
+                $account->lockout = new stdClass();
+                $account->lockout->attempts = 0;
+                $account->name = $name;
+                $account->saltA = salt();
+                $account->saltB = salt();
+                $account->hashed = hashed($password, $account->saltA, $account->saltB);
+                $id = id();
+                $database->$id = $account;
+                result("register", "success", true);
+            } else {
+                error("register", "Password too short");
+            }
+        } else {
+            error("register", "Name already taken");
+        }
+    } else {
+        error("register", "Registration disabled");
+    }
+}
+
+function result($type, $key, $value)
+{
+    global $result;
+    if (!isset($result->$type)) $result->$type = new stdClass();
+    $result->$type->$key = $value;
+}
+
+function salt()
+{
+    return random(128);
+}
+
+function save()
+{
+    global $database;
+    file_put_contents(DATABASE, json_encode($database));
+}
+
+function user($id)
+{
+    global $database;
+    $user = $database->$id;
+    $user->id = $id;
+    unset($user->saltA);
+    unset($user->saltB);
+    unset($user->hashed);
+    unset($user->certificates);
+    return $user;
+}
+
+function verify($certificate)
+{
+    global $database;
+    result("verify", "success", false);
+    foreach ($database as $id => $account) {
+        foreach ($account->certificates as $current) {
+            if ($current === $certificate) {
+                result("verify", "name", $account->name);
+                result("verify", "success", true);
+                return user($id);
+            }
+        }
+    }
+    return null;
+}
+
---
+++ scripts/frontend/accounts/accounts.js
@@ -0,0 +1,133 @@
+const certificateCookie = "certificate";
+let success, failure;
+
+function accounts(callback) {
+    view("accounts");
+    success = (loggedIn = false) => {
+        hide("accounts");
+        callback(loggedIn);
+    };
+    failure = () => view("login");
+    if (hasCookie(certificateCookie))
+        verify(success, failure);
+    else
+        view("login");
+}
+
+function fillForm(form = new FormData()) {
+    if (hasCookie(certificateCookie)) {
+        form.append("action", "verify");
+        form.append("verify", JSON.stringify({certificate: pullCookie(certificateCookie)}));
+    }
+    return form;
+}
+
+function force() {
+    success();
+}
+
+function hasCookie(name) {
+    return pullCookie(name) !== undefined;
+}
+
+function login(name, password) {
+
+    function error(error) {
+        get("login-error").innerText = error;
+    }
+
+    let form = new FormData();
+    form.append("action", "login");
+    form.append("login", JSON.stringify({name: name, password: password}));
+    fetch("scripts/backend/accounts/accounts.php", {
+        method: "post",
+        body: form
+    }).then(response => {
+        response.text().then((result) => {
+            let json = JSON.parse(result);
+            if (json.hasOwnProperty("login")) {
+                if (json.login.hasOwnProperty("success")) {
+                    if (json.login.success) {
+                        if (json.login.hasOwnProperty("certificate")) {
+                            pushCookie(certificateCookie, json.login.certificate);
+                            window.location.reload();
+                        }
+                    }
+                }
+            }
+            if (json.hasOwnProperty("errors") && json.errors.hasOwnProperty("login")) error(json.errors.login);
+        });
+    });
+}
+
+function pullCookie(name) {
+    name += "=";
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(name) === 0) {
+            return decodeURIComponent(cookie.substring(name.length, cookie.length));
+        }
+    }
+    return undefined;
+}
+
+function pushCookie(name, value) {
+    const date = new Date();
+    date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000));
+    document.cookie = name + "=" + encodeURIComponent(value) + ";expires=" + date.toUTCString() + ";domain=" + window.location.hostname + ";path=/";
+}
+
+function register(name, password) {
+
+    function error(error) {
+        get("register-error").innerText = error;
+    }
+
+    let form = new FormData();
+    form.append("action", "register");
+    form.append("register", JSON.stringify({name: name, password: password}));
+    fetch("scripts/backend/accounts/accounts.php", {
+        method: "post",
+        body: form
+    }).then(response => {
+        response.text().then((result) => {
+            let json = JSON.parse(result);
+            if (json.hasOwnProperty("register")) {
+                if (json.register.hasOwnProperty("success")) {
+                    if (json.register.success) {
+                        login(name, password);
+                    }
+                }
+            }
+            if (json.hasOwnProperty("errors") && json.errors.hasOwnProperty("register")) error(json.errors.register);
+        });
+    });
+}
+
+function verify(success, failure) {
+    let form = fillForm();
+    fetch("scripts/backend/accounts/accounts.php", {
+        method: "post",
+        body: form
+    }).then(response => {
+        response.text().then((result) => {
+            let json = JSON.parse(result);
+            if (json.hasOwnProperty("verify")) {
+                if (json.verify.hasOwnProperty("success")) {
+                    if (json.verify.success) {
+                        view("app");
+                        success(true);
+                    } else {
+                        failure();
+                    }
+                }
+            } else {
+                failure();
+            }
+        });
+    });
+}
