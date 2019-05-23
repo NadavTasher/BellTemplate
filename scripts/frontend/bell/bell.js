7666226ec83827d10eadaea3c5c9fddd836d0f08
Nadav Tasher <tashernadav@gmail.com>
2019-05-23 18:16:30 +0300

    THings

--- scripts/frontend/bell/bell.js
+++ scripts/frontend/bell/bell.js
@@ -7,7 +7,7 @@ function bell(loggedIn) {
     loadDatabase();
 }
 
-function loadDatabase() {
+function loadDatabase(callback = undefined) {
     fetch("files/bell/database.json", {
         method: "get"
     }).then(response => {
@@ -15,8 +15,8 @@ function loadDatabase() {
             database = JSON.parse(result);
             updateGeneral();
             updatePresets();
-            updateTimes();
             updateLibrary();
+            if (callback !== undefined) callback();
         });
     });
 }
@@ -28,12 +28,36 @@ function loadPreset(name) {
         for (let key in database.queue) {
             if (database.queue.hasOwnProperty(key)) {
                 let div = document.createElement("div");
-                div.classList.add("sideways");
                 let time = document.createElement("p");
+                let select = document.createElement("select");
+                let second = document.createElement("input");
                 let minute = parseInt(key);
-                time.innerText = (minute - minute % 60) / 60 + ":" + (minute % 60 < 10) ? ("0" + minute % 60) : (minute % 60);
+                div.classList.add("sideways");
+                time.innerText = ((minute - minute % 60) / 60) + ":" + ((minute % 60 < 10) ? ("0" + minute % 60) : (minute % 60));
+                second.type = "number";
+                second.placeholder = "Second";
+                if (database.hasOwnProperty("library")) {
+                    for (let key in database.library) {
+                        if (database.library.hasOwnProperty(key)) {
+                            let value = database.library[key];
+                            if (value.hasOwnProperty("name")) {
+                                let media = document.createElement("option");
+                                media.innerText = value.name;
+                                media.value = key;
+                                select.appendChild(media);
+                            }
+                        }
+                    }
+                }
                 let value = database.queue[key];
+                if (value.hasOwnProperty(name)) {
+                    select.value = value[name];
+                } else {
+                    select.value = undefined;
+                }
                 div.appendChild(time);
+                div.appendChild(select);
+                div.appendChild(second);
                 get("preset-queue").appendChild(div);
             }
         }
@@ -47,6 +71,7 @@ function muteTextForState(state) {
 function reload() {
     window.location.reload(true);
 }
+
 function save(command, parameters, callback = undefined, form = fillForm()) {
     form.append("bell", JSON.stringify({
         action: command,
@@ -75,26 +100,7 @@ function setDuration(duration) {
 
 function setMute(state) {
     get("mute-state").innerText = muteTextForState(state);
-    save("mute", {mute: state}, () => {
-    });
-}
-
-function update() {
-
-
-    if (json.hasOwnProperty("presets")) {
-        for (let i = 0; i < json.presets.length; i++) {
-            let preset = document.createElement("option");
-            preset.value = json.presets[i];
-            preset.innerText = json.presets[i];
-            get("preset-selector").appendChild(preset);
-
-            let queue = document.createElement("div");
-            queue.id = "preset-queue-" + json.presets[i];
-
-        }
-        get("preset-selector").value = json.hasOwnProperty("preset") ? json.preset : "";
-    }
+    save("mute", {mute: state});
 }
 
 function updateDuration() {
@@ -114,9 +120,11 @@ function updateLibrary() {
         for (let key in database.library) {
             if (database.library.hasOwnProperty(key)) {
                 let value = database.library[key];
-                let media = document.createElement("p");
-                media.innerText = value.name;
-                get("library-list").appendChild(media);
+                if (value.hasOwnProperty("name")) {
+                    let media = document.createElement("p");
+                    media.innerText = value.name;
+                    get("library-list").appendChild(media);
+                }
             }
         }
     }
@@ -138,18 +146,75 @@ function updatePresets() {
             option.value = value;
             get("preset-list").appendChild(option);
         }
+        if (database.hasOwnProperty("preset"))
+            get("preset-list").value = database.preset;
         get("preset-list").oninput();
     }
 }
 
-function updateTimes() {
+function addTime() {
+    let div = document.createElement("div");
+    let title = document.createElement("p");
+    let time = document.createElement("div");
+    let buttons = document.createElement("div");
+    let ok = document.createElement("button");
+    let cancel = document.createElement("button");
+    let hour = document.createElement("input");
+    let minute = document.createElement("input");
+    let colon = document.createElement("p");
+    buttons.classList.add("sideways");
+    time.classList.add("sideways");
+    time.appendChild(hour);
+    time.appendChild(colon);
+    time.appendChild(minute);
+    buttons.appendChild(ok);
+    buttons.appendChild(cancel);
+    title.classList.add("title");
+    title.innerText = "Add time";
+    colon.innerText = ":";
+    colon.style.width = "min-content";
+    hour.type = "number";
+    hour.min = 0;
+    hour.max = 23;
+    hour.placeholder = "Hour";
+    minute.type = "number";
+    minute.min = 0;
+    minute.max = 59;
+    minute.placeholder = "Minute";
+    ok.innerText = "Add";
+    cancel.innerText = "Cancel";
+    ok.onclick = () => {
+        save("addTime", {time: (parseInt(hour.value) * 60 + parseInt(minute.value))}, () => {
+            loadDatabase(() => {
+                view("general");
+            });
+        });
+    };
+    cancel.onclick = () => {
+        view("general");
+    };
+    div.appendChild(title);
+    div.appendChild(time);
+    div.appendChild(buttons);
+    prompt(div);
+}
 
+function prompt(content) {
+    clear("prompt");
+    get("prompt").appendChild(content);
+    view("prompt");
 }
 
 function uploadMedia() {
     let form = fillForm();
     form.append("audio", get("library-add-file").files[0]);
-    save("upload", {name: get("library-add-name").value}, (result) => {
-        if (result.hasOwnProperty("success") && result.success) reload();
-    }, form);
+    if (get("library-add-name").value.length > 0) {
+        save("upload", {name: get("library-add-name").value}, (result) => {
+            if (result.hasOwnProperty("success") && result.success) {
+                loadDatabase(() => {
+                    view("library");
+                });
+            }
+        }, form);
+    }
 }
