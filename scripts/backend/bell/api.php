19704492eac2f32c4cd9127e31e16f5007e8eb7b
Nadav Tasher <tashernadav@gmail.com>
2019-06-08 13:05:53 +0300

    Inherited Accounts Update / Struct Update

--- scripts/backend/bell/api.php
+++ scripts/backend/bell/api.php
@@ -4,112 +4,106 @@ include_once __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "accou
 
 const BELL_API = "bell";
 
-const DATABASE = __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "files" . DIRECTORY_SEPARATOR . "bell" . DIRECTORY_SEPARATOR . "database.json";
-const MEDIA_DIRECTORY = __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "files" . DIRECTORY_SEPARATOR . "media";
+const BELL_DATABASE = __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "files" . DIRECTORY_SEPARATOR . "bell" . DIRECTORY_SEPARATOR . "database.json";
+const BELL_MEDIA_DIRECTORY = __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "files" . DIRECTORY_SEPARATOR . "media";
 
-$database = json_decode(file_get_contents(DATABASE));
+$bell_database = json_decode(file_get_contents(BELL_DATABASE));
 
 function bell()
 {
     $user = accounts();
     if ($user !== null) {
-        if (isset($_POST[BELL_API])) {
-            $information = json_decode(filter($_POST[BELL_API]));
-            if (isset($information->action) && isset($information->parameters)) {
-                $action = $information->action;
-                $parameters = $information->parameters;
-                result(BELL_API, $action, "success", false);
-                switch ($action) {
-                    case "media-add":
-                        if (isset($parameters->name)) {
-                            if (bell_ends_with($_FILES["audio"]["tmp_name"], ".mp3")) {
-                                $file = random(30) . ".mp3";
-                                move_uploaded_file($_FILES["audio"]["tmp_name"], MEDIA_DIRECTORY . DIRECTORY_SEPARATOR . $file);
-                                bell_media_add($parameters->name, $file);
-                                result(BELL_API, $action, "success", true);
-                            } else {
-                                error(BELL_API, $action, "Wrong media format");
-                            }
+        api(BELL_API, function ($action, $parameters) {
+            switch ($action) {
+                case "media-add":
+                    if (isset($parameters->name)) {
+                        if (bell_ends_with($_FILES["audio"]["tmp_name"], ".mp3")) {
+                            $file = random(30) . ".mp3";
+                            move_uploaded_file($_FILES["audio"]["tmp_name"], BELL_MEDIA_DIRECTORY . DIRECTORY_SEPARATOR . $file);
+                            bell_media_add($parameters->name, $file);
+                            return [true, null];
                         } else {
-                            error(BELL_API, $action, "Missing information");
+                            return [false, "Wrong media format"];
                         }
-                        break;
-                    case "time-add":
-                        if (isset($parameters->time)) {
-                            bell_time_add($parameters->time);
-                            result(BELL_API, $action, "success", true);
-                        } else {
-                            error(BELL_API, $action, "Missing information");
-                        }
-                        break;
-                    case "time-remove":
-                        if (isset($parameters->time)) {
-                            bell_time_remove($parameters->time);
-                            result(BELL_API, $action, "success", true);
-                        } else {
-                            error(BELL_API, $action, "Missing information");
-                        }
-                        break;
-                    case "preset-add":
-                        if (isset($parameters->preset)) {
-                            bell_preset_add($parameters->preset);
-                            result(BELL_API, $action, "success", true);
-                        } else {
-                            error(BELL_API, $action, "Missing information");
-                        }
-                        break;
-                    case "preset-remove":
-                        if (isset($parameters->preset)) {
-                            bell_preset_remove($parameters->preset);
-                            result(BELL_API, $action, "success", true);
-                        } else {
-                            error(BELL_API, $action, "Missing information");
-                        }
-                        break;
-                    case "preset-set":
-                        if (isset($parameters->preset)) {
-                            bell_preset_set($parameters->preset);
-                            result(BELL_API, $action, "success", true);
-                        } else {
-                            error(BELL_API, $action, "Missing information");
-                        }
-                        break;
-                    case "queue-add":
-                        if (isset($parameters->time) && isset($parameters->preset) && isset($parameters->media) && isset($parameters->second)) {
-                            bell_queue_add($parameters->time, $parameters->preset, $parameters->media, $parameters->second);
-                            result(BELL_API, $action, "success", true);
-                        } else {
-                            error(BELL_API, $action, "Missing information");
-                        }
-                        break;
-                    case "queue-remove":
-                        if (isset($parameters->time) && isset($parameters->preset)) {
-                            bell_queue_remove($parameters->time, $parameters->preset);
-                            result(BELL_API, $action, "success", true);
-                        } else {
-                            error(BELL_API, $action, "Missing information");
-                        }
-                        break;
-                    case "mute-set":
-                        if (isset($parameters->mute)) {
-                            bell_mute_set($parameters->mute);
-                            result(BELL_API, $action, "success", true);
-                        } else {
-                            error(BELL_API, $action, "Missing information");
-                        }
-                        break;
-                    case "duration-set":
-                        if (isset($parameters->duration)) {
-                            bell_duration_set($parameters->duration);
-                            result(BELL_API, $action, "success", true);
-                        } else {
-                            error(BELL_API, $action, "Missing information");
-                        }
-                        break;
-                }
-                bell_save();
+                    } else {
+                        return [false, "Missing information"];
+                    }
+                    break;
+                case "time-add":
+                    if (isset($parameters->time)) {
+                        bell_time_add($parameters->time);
+                        return [true, null];
+                    } else {
+                        return [false, "Missing information"];
+                    }
+                    break;
+                case "time-remove":
+                    if (isset($parameters->time)) {
+                        bell_time_remove($parameters->time);
+                        return [true, null];
+                    } else {
+                        return [false, "Missing information"];
+                    }
+                    break;
+                case "preset-add":
+                    if (isset($parameters->preset)) {
+                        bell_preset_add($parameters->preset);
+                        return [true, null];
+                    } else {
+                        return [false, "Missing information"];
+                    }
+                    break;
+                case "preset-remove":
+                    if (isset($parameters->preset)) {
+                        bell_preset_remove($parameters->preset);
+                        return [true, null];
+                    } else {
+                        return [false, "Missing information"];
+                    }
+                    break;
+                case "preset-set":
+                    if (isset($parameters->preset)) {
+                        bell_preset_set($parameters->preset);
+                        return [true, null];
+                    } else {
+                        return [false, "Missing information"];
+                    }
+                    break;
+                case "queue-add":
+                    if (isset($parameters->time) && isset($parameters->preset) && isset($parameters->media) && isset($parameters->second)) {
+                        bell_queue_add($parameters->time, $parameters->preset, $parameters->media, $parameters->second);
+                        return [true, null];
+                    } else {
+                        return [false, "Missing information"];
+                    }
+                    break;
+                case "queue-remove":
+                    if (isset($parameters->time) && isset($parameters->preset)) {
+                        bell_queue_remove($parameters->time, $parameters->preset);
+                        return [true, null];
+                    } else {
+                        return [false, "Missing information"];
+                    }
+                    break;
+                case "mute-set":
+                    if (isset($parameters->mute)) {
+                        bell_mute_set($parameters->mute);
+                        return [true, null];
+                    } else {
+                        return [false, "Missing information"];
+                    }
+                    break;
+                case "duration-set":
+                    if (isset($parameters->duration)) {
+                        bell_duration_set($parameters->duration);
+                        return [true, null];
+                    } else {
+                        return [false, "Missing information"];
+                    }
+                    break;
             }
-        }
+            return [false, null];
+        }, true);
     }
     return null;
 }
@@ -118,28 +112,31 @@ function bell()
 
 function bell_mute_set($mute)
 {
-    global $database;
-    $database->mute = $mute;
+    global $bell_database;
+    $bell_database->mute = $mute;
+    bell_save();
 }
 
 function bell_duration_set($duration)
 {
-    global $database;
-    $database->duration = $duration;
+    global $bell_database;
+    $bell_database->duration = $duration;
+    bell_save();
 }
 
 // Combined management
 
 function bell_queue_add($time, $preset, $media, $second)
 {
-    global $database;
+    global $bell_database;
     if (bell_time_exists($time)) {
         if (bell_preset_exists($preset)) {
             if (bell_media_exists($media)) {
                 $artifact = new stdClass();
                 $artifact->media = $media;
                 $artifact->second = is_numeric($second) ? $second : doubleval($second);
-                $database->queue->$time->$preset = $artifact;
+                $bell_database->queue->$time->$preset = $artifact;
+                bell_save();
             }
         }
     }
@@ -147,11 +144,12 @@ function bell_queue_add($time, $preset, $media, $second)
 
 function bell_queue_remove($time, $preset)
 {
-    global $database;
+    global $bell_database;
     if (bell_time_exists($time)) {
         if (bell_preset_exists($preset)) {
-            if (isset($database->queue->$time->$preset)) {
-                unset($database->queue->$time->$preset);
+            if (isset($bell_database->queue->$time->$preset)) {
+                unset($bell_database->queue->$time->$preset);
+                bell_save();
             }
         }
     }
@@ -161,41 +159,44 @@ function bell_queue_remove($time, $preset)
 
 function bell_preset_add($name)
 {
-    global $database;
+    global $bell_database;
     if (!bell_preset_exists($name)) {
-        array_push($database->presets, $name);
+        array_push($bell_database->presets, $name);
+        bell_save();
     }
 }
 
 function bell_preset_remove($name)
 {
-    global $database;
+    global $bell_database;
     if (bell_preset_exists($name)) {
         $presets = array();
-        foreach ($database->presets as $preset) {
+        foreach ($bell_database->presets as $preset) {
             if ($preset !== $name) {
                 array_push($presets, $preset);
             }
         }
-        foreach ($database->queue as $time) {
+        foreach ($bell_database->queue as $time) {
             if (isset($time->$name)) unset($time->$name);
         }
-        $database->presets = $presets;
+        $bell_database->presets = $presets;
+        bell_save();
     }
 }
 
 function bell_preset_set($name)
 {
-    global $database;
+    global $bell_database;
     if (bell_preset_exists($name)) {
-        $database->preset = $name;
+        $bell_database->preset = $name;
+        bell_save();
     }
 }
 
 function bell_preset_exists($name)
 {
-    global $database;
-    foreach ($database->presets as $preset) {
+    global $bell_database;
+    foreach ($bell_database->presets as $preset) {
         if ($name === $preset) return true;
     }
     return false;
@@ -205,43 +206,46 @@ function bell_preset_exists($name)
 
 function bell_media_add($name, $file)
 {
-    global $database;
+    global $bell_database;
     $mediaID = random(20);
     $media = new stdClass();
     $media->name = $name;
     $media->media = $file;
-    $media->hash = sha1_file(MEDIA_DIRECTORY . DIRECTORY_SEPARATOR . $file);
-    $database->library->$mediaID = $media;
+    $media->hash = sha1_file(BELL_MEDIA_DIRECTORY . DIRECTORY_SEPARATOR . $file);
+    $bell_database->library->$mediaID = $media;
+    bell_save();
 }
 
 function bell_media_exists($id)
 {
-    global $database;
-    return isset($database->library->$id);
+    global $bell_database;
+    return isset($bell_database->library->$id);
 }
 
 // Time management
 
 function bell_time_add($time)
 {
-    global $database;
+    global $bell_database;
     if (!bell_time_exists($time)) {
-        $database->queue->$time = new stdClass();
+        $bell_database->queue->$time = new stdClass();
+        bell_save();
     }
 }
 
 function bell_time_remove($time)
 {
-    global $database;
+    global $bell_database;
     if (bell_time_exists($time)) {
-        unset($database->queue->$time);
+        unset($bell_database->queue->$time);
+        bell_save();
     }
 }
 
 function bell_time_exists($time)
 {
-    global $database;
-    return isset($database->queue->$time);
+    global $bell_database;
+    return isset($bell_database->queue->$time);
 }
 
 
@@ -257,6 +261,6 @@ function bell_ends_with($haystack, $needle)
 
 function bell_save()
 {
-    global $database;
-    file_put_contents(DATABASE, json_encode($database));
+    global $bell_database;
+    file_put_contents(BELL_DATABASE, json_encode($bell_database));
 }
